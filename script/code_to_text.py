46fcdada5492594785791fcf790b3cbbdb8a9a0d
Michael Franks <mffranks34@gmail.com>
2026-03-12 11:38:58 +0000

    adding some files for script that are going to be mad

---
+++ script/code_to_text.py
@@ -0,0 +1,16 @@
+"""
+A System to turn python code into text files
+For Assignment Submission
+Author: Michael Franks
+"""
+
+PROJECT_DIR = "/Workspace/ros2_ws/src"
+
+class CodeToText:
+    pass
+
+def run():
+    pass
+
+if __name__ == '__main__':
+    pass
---
+++ script/run_simulation.py
@@ -0,0 +1,10 @@
+"""
+System Designed Run the Robot Simulation
+Author: Michael Franks
+"""
+
+def run_simulation():
+    pass
+
+if __name__ == '__main__':
+    run_simulation()
