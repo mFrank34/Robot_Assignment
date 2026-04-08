93a002636ff8e8febe2f199301ce1ca6eeface4d
Michael Frank <mffranks34@gmail.com>
2026-04-08 18:07:26 +0000

    update maze

--- script/run.py
+++ script/run.py
@@ -14,21 +14,48 @@ information = [
     " 3. Run Client"
 ]
 
+simulation = [
+    "1. Maze",
+    "2. Hallway",
+    "3. Free Roam"
+]
+
 
 def run(command):
     os.system(f'bash -c "{command}; exec bash"')
     time.sleep(1)
 
 
+def run_sim():
+    print("\n--- Select World ---")
+    for index in simulation:
+        print(index)
+    user_input = input("Enter your command: ")
+    os.system("pkill -9 -f ign 2>/dev/null")
+    match user_input:
+        case "1":
+            print("Starting Maze Simulation...")
+            run("ign gazebo ~/gazebo_projects/worlds/maze.sdf")
+        case "2":
+            print("Starting Hallway Simulation...")
+            run("ign gazebo ~/gazebo_projects/worlds/hallway.sdf")
+        case "3":
+            print("Starting Free Roam Simulation...")
+            run("ign gazebo ~/gazebo_projects/worlds/free_roam.sdf")
+        case _:
+            print("Invalid Input")
+
+
 def start():
+    print("\n=== Main Menu ===")
+
     for index in information:
         print(index)
 
     user_input = input("Enter your command: ")
     match user_input:
         case "1":
-            print("Starting Gazebo Simulation...")
-            run("ign gazebo ~/gazebo_projects/worlds/hallway.sdf")
+            run_sim()
         case "2":
             print("Starting Server & bridge...")
             run("source ~/ros2_ws/install/setup.bash && ros2 launch robot launch.py")
